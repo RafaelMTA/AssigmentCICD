eafa191c350dcfc8fa7aea2e9fc7fe711b8c2b88
MyName <teste@teste.com>
2023-03-14 22:25:54 +1300

    Add project files.

---
+++ AssigmentCICD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AssigmentCICD.Models;
+
+namespace AssigmentCICD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<AssigmentCICD.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ AssigmentCICD/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssigmentCICD.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
